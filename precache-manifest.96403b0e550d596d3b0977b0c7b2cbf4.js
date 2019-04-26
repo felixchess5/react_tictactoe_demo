6899c012befa18828b053674a6a03b559b2ed5d7
Elias <felix.elias@accenture.com>
2019-04-26 13:39:51 -0400

    Updates

---
+++ precache-manifest.96403b0e550d596d3b0977b0c7b2cbf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd7696ab64a24b7c036b222db94d77f",
+    "url": "/felixchess5/react_tictactoe_demo.git/index.html"
+  },
+  {
+    "revision": "fa5468d081bb0b68c95e",
+    "url": "/felixchess5/react_tictactoe_demo.git/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/felixchess5/react_tictactoe_demo.git/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "fa5468d081bb0b68c95e",
+    "url": "/felixchess5/react_tictactoe_demo.git/static/js/main.3dacae77.chunk.js"
+  },
+  {
+    "revision": "5d926dc186df17012d90",
+    "url": "/felixchess5/react_tictactoe_demo.git/static/js/runtime~main.4b845b9f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/felixchess5/react_tictactoe_demo.git/precache-manifest.96403b0e550d596d3b0977b0c7b2cbf4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/felixchess5/react_tictactoe_demo.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
